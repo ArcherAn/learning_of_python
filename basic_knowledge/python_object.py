5ef167e9d52120043ae2927491a5d2ed9a037513
ArcherAn <Archer_An@163.com>
2017-04-12 11:13:48 +0800

    2017-04-12

--- basic_knowledge/python_object.py
+++ basic_knowledge/python_object.py
@@ -35,3 +35,89 @@ employee = Employee("sss", 234)
 employee.displayCount()
 employee.displayEmployee()
 
+# python的内置属性
+"""
+1、__dict__:类的属性（包含一个字典，由类的数据属性组成）
+2、__doc__:类的文档字符串
+3、__name__:类名
+4、__module__:类定义所在的模块（类全名是'__main__.className',如果类位于一个导入模块mymod中，
+  那么calssName.__module__等于mymod)
+5、__bases__:类的所有父类构成元素（包含类一个由所有父类组成的元组）
+"""
+
+# python的内置类属性调用实例
+print "Employee.__doc__:", Employee.__doc__
+print "Employee.__name__", Employee.__name__
+print "Employee.__module__", Employee.__module__
+print "Employee.__bases__", Employee.__bases__
+print "Employee.__dict__", Employee.__dict__
+
+
+# 析构函数__del__，在对象被销毁时调用
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+    def __del__(self):
+        class_name = self.__class__.__name__
+        print class_name, "销毁"
+pt1 = Point()
+pt2 = pt1
+pt3 = pt1
+print id(pt1), id(pt2), id(pt3)    # 打印对象id
+del pt1
+del pt2
+del pt3
+
+# 类的继承，通过继承实现代码重用
+"""
+1、在继承中基类的构造（__init__()方法）不会被自动调用，它需要在其派生类的构造中专门调用
+2、在调用基类的方法时，需要加上基类的类名前缀，且需要带上self参数变量。区别于在类中调用函数时
+   并不需要带上self参数。
+3、python总是首先查找对应类型的方法，如果它不能在派生类中找到对应的方法，它才开始到基类中逐个查找。
+  (首先在本类中查找调用方法，找不到才去基类中找）
+4、如果在继承元组中列了一个以上的类，那么它就被称作"多重继承"
+"""
+
+#   继承实例
+
+class Parent:         # 定义父类
+    parentAttr = 100
+    def __init__(self):
+        print "调用父类构造函数"
+
+    def parentMethod(self):
+        print "调用父类方法"
+
+    def setAttr(self, attr):
+        Parent.parentAttr = attr
+
+    def getAttr(self):
+        print "父类属性：", Parent.parentAttr
+
+class Child(Parent):    # 定义子类
+    def __init__(self):
+        print "调用子类构造方法"
+
+    def childMethod(self):
+        print "调用子类方法 child method"
+
+c = Child()       # 实例化子类
+c.childMethod()   # 调用子类方法
+c.parentMethod()  # 调用父类方法
+c.setAttr(200)    # 再次调用父类方法
+c.getAttr()       # 再次调用父类方法
+
+# 方法重写
+class Father:
+    def myMethod(self):
+        print "调用父类方法"
+
+class Son(Father):
+    def myMethod(self):
+        print "调用子类方法"
+
+s = Son()
+s.myMethod()
+
+# 类中的变量名前面加上__表示私有变量
